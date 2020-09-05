64aa750ba9082e6f5de2243dcb875393ba97e89b
Ismail Mohammed <ismailmo4@gmail.com>
2020-09-05 21:25:46 +0100

    separated modules

--- img_processing.py
+++ img_processing.py
@@ -1,9 +1,43 @@
-
 import cv2
 import numpy as np
-import pandas as pd
-import copy
-import json
+from pdf2image import convert_from_path
+from pathlib import Path
+import os
+
+def pdfFolderToImgs(pdfFolderPath, outputLoc='pdfFolder', deletePdf = True, popplerPath=r"C:\Users\ismail.mohammed\poppler-0.68.0\bin"):
+    '''
+    returns list of img paths
+    convert all pdfs in pdfFolderPath to imgs with pdfFileName + page number in outputLoc
+    popplerPath = location of \poppler-0.68.0\bin
+                (ensure poppler is installed and in path)    
+    '''
+    pdfPaths = [file for file in os.listdir(pdfFolderPath) if file.endswith('.pdf')]
+    if outputLoc == 'pdfFolder':
+        outputLoc = Path(pdfFolderPath)
+    else:
+        outputLoc = Path(outputLoc)
+            
+    imgPaths =[]
+    # #saving pdf as images
+    for pdf in pdfPaths:
+        imgNum = 1
+        pdfPath= Path.joinpath(Path(pdfFolderPath), Path(pdf))
+        pages = convert_from_path(pdfPath, poppler_path=popplerPath)
+        for i,p in enumerate(pages):
+            #if rotation needed? - shouldnt need if scanned properly - (maybe add label to sheet: SCAN THIS WAY ->)
+            #p = p.rotate(180, expand=True)            
+            imgPath = Path.joinpath(outputLoc, str(pdf.split('.')[0]) +'pg_' + str(imgNum) +".jpg")
+            p.save(imgPath)
+            imgPaths.append(imgPath)
+            imgNum+=1
+    
+    #delete pdf files 
+    if deletePdf:
+        for pdf in pdfPaths:
+            os.remove(Path.joinpath(Path(pdfFolderPath), Path(pdf)))
+    
+    #conv path obj to strings (cv2 needs paths as strings)    
+    return [str(imgPath) for imgPath in imgPaths]
 
 def showImg(img, time=0):
     '''img = single np.array or list of np.array imgs
@@ -25,14 +59,13 @@ def threshImg(img, minThresh=100, maxThresh=255):
     
     return imgThresh
 
-
-
-def alignForm(img, width=0, height=0, template = False):
+#img_processing.py
+def alignForm(img, width=0, height=0, isTemplate = False):
     '''apply perspective transformation to align form with template
     img = input form as np.array (use cv2.imread)
-    width = use template image width (i.e. templateImg.shape[1])
-    height = use template image height (i.e. templateImg.shape[0])
-    template = if defining template: set to True to ensure the image is scaled down to 
+    width = use template image width (i.e. template['metadata']['shape'][1])
+    height = use template image height (i.e. template['metadata']['shape'][0])
+    isTemplate = if defining template: set to True to ensure the image is scaled down to 
     fit screen when showing image, will override width and height with euclidean distances of outer border'''
     
 
@@ -41,7 +74,7 @@ def alignForm(img, width=0, height=0, template = False):
     #only resize if template - otherwise image will be resized with warp transform anyways
     xScale = 0.5
     yScale = 0.5
-    if template == True:
+    if isTemplate == True:
         imgResize = cv2.resize(img, (0,0), fx= xScale, fy=yScale, interpolation= cv2.INTER_AREA)
         print("AlignImage: img resized by {}{} in width and {}{} in height".format(int(xScale*100),'%', int(yScale*100), '%'))
     else:
@@ -99,11 +132,11 @@ def alignForm(img, width=0, height=0, template = False):
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 
     #find max heights/widths - i.e from top/bottom, left/right
-    if width == 0 or template==True:
+    if width == 0 or isTemplate==True:
         maxWidth = max(int(widthA), int(widthB))
     else:
         maxWidth = width
-    if height == 0 or template==True:
+    if height == 0 or isTemplate==True:
         maxHeight = max(int(heightA), int(heightB))
     else:
         maxHeight = height
@@ -120,72 +153,3 @@ def alignForm(img, width=0, height=0, template = False):
 
     #transform image and resize to original size (map spots to correct locations)
     return cv2.warpPerspective(imgResize, matrix, (maxWidth, maxHeight))
-
-## access answers of question (use this to check filled status from image?)
-
-def getQuestionDetails(questionString, templateList):
-    '''return list of answer dictionaries for a question'''
-    index = [ind for ind, qn in enumerate(templateList) if qn['question']==questionString]
-    
-    if len(index)==1:
-        index = index[0]
-    else:
-        raise Exception("{} questions returned!".format(len(index)))
-    return templateList[index]['answers']
-
-### define generic function to generate questions for template
-
-def generateQuestion(xRange, yRange, orient, questionName, answerValues, spotCoordsList):
-    '''returns question dictionary 
-    xRange/yRange = list of x and y coordinates
-    orient = 'column' or 'row' or 'grid'
-    questionName = str of question to associate with answerValues
-    answerValues = list of values to associate with coordinates in xRange, 
-    must be sorted from top to bottom (if orient = 'column'),
-    left to right (if orient = 'row')
-    left to right, then top to bottom - like reading english (if orient = 'grid')
-    spotCoordsList = list of all coordinates from template to extract spots within xRange and yRange from.
-    '''
-    #return list of coordinates within range
-    coordList = [i for i in spotCoordsList if xRange[0]<=i[0]<=xRange[1] and yRange[0]<=i[1]<=yRange[1]]
-    
-    #sort list by x if orient == 'row' or by y if column
-    if orient =='row':
-        coordList = sorted(coordList, key = lambda x:x[0])
-    elif orient =='column':
-        coordList = sorted(coordList, key = lambda x:x[1])
-    elif orient=='grid':
-        coordList = sorted(coordList,key =  lambda x:(x[1], x[0]))
-    else:
-        raise Exception("orient must be 'column', 'row', or 'grid'")
-    
-    
-    #map coordinates to values
-    if len(answerValues) == len(coordList):
-        valueCoords = zip(answerValues, coordList)
-    else:
-        raise Exception("answerValues length: ({}) must match coordList length: ({})".format(len(answerValues), len(coordList)))
-    
-    #add question to template
-    questionDict = {'question': questionName, 'answers':[]}
-    
-    #add in answers
-    for val, coord in valueCoords:
-        questionDict['answers'].append({'value': val, 'coord':coord, 'filled': False})
-    
-    return questionDict
-
-## helper function to visualise questions/values
-
-def showQuestion(questionString, templateList, img, fontSize, time=0, show=True):
-    questionDetails = getQuestionDetails(questionString, templateList)
-    questionLocX = questionDetails[0]['coord'][0]-10
-    questionLocY = questionDetails[0]['coord'][1]-10
-    #put text of question
-    cv2.putText(img, questionString, (questionLocX, questionLocY), cv2.FONT_HERSHEY_SIMPLEX, fontSize*1.5, (0,0,0))
-    for qn in questionDetails:
-        x,y = qn['coord']        
-        cv2.circle(img, (x,y), 5, (0,0,255), -1)        
-        cv2.putText(img, str(qn['value']), (x+10, y), cv2.FONT_HERSHEY_SIMPLEX, fontSize, (0,0,0))
-    if show:
-        showImg(img, time)
--- main.py
+++ main.py
@@ -1,95 +1,27 @@
-import ismail_omr as imomr
-import ismail_pdfImg as imopdf
+import form_read as fr
+import img_processing as ip
 import os
 import argparse
+import pandas as pd
 
-### need to add in template height and width into variable or hardcode in to replace tempImgAligned.shape[0] and [1] - maybe have default value for alignform func??#
-#add metadata into template file? - temp width, height, 
-## wrap image processing in separate function and put spot size and pctfilled values as arguments##
-##for image in pdf file:
-##  align image, rotate, threshold
-##  perform omr funct
-##  find answers for each qn
-##  return dataframe
 ##  add in analysis script into separate module
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pdfFolder")
+    parser.add_argument("templatePath")
+    parser.add_argument("csvPath")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("pdf")
-parser.add_argument("template")
-args = parser.parse_args()
+    #convert pdf to imgs and return path list
+    pdfImgList = ip.pdfFolderToImgs(args.pdfFolder, popplerPath= r"C:\Users\ismail\poppler-0.68.0_x86\poppler-0.68.0\bin")
+    
+    df = fr.omrRead(pdfImgList, args.templatePath)
+    
+    #import as csv as temporary measure
+    #in future run analysis code here
+    df.to_csv(args.csvPath, index=False)
 
-with open(args.template, 'r') as template:
-    jsonTempLoad = json.load(template)
+if __name__ == '__main__':
+    main()
 
-
-## overlay template mask onto forms and find filled in spots
-
-imgList = [os.path.join("scanTrials", i) for i in os.listdir(r"scanTrials") if i.endswith(".jpg")]
-
-finalAnswers=[]
-
-for formPath in imgList:
-    templateSpots =copy.deepcopy(jsonTempLoad)
-    scannedFormImg =cv2.imread(formPath)
-    if scannedFormImg.shape[0]>scannedFormImg.shape[1]:
-        scannedFormImg=cv2.rotate(scannedFormImg, cv2.ROTATE_90_CLOCKWISE)
-    alignedScanImg = imomr.alignForm(scannedFormImg, height= tempImgAligned.shape[0], width= tempImgAligned.shape[1], template=False)    
-    alignedScanImgThresh = imomr.threshImg(alignedScanImg)
-
-    #showImg(alignedScanImgThresh)
-
-    filledThreshold =0.4
-    circleSize = 6
-
-    omrRead =[]
-
-    for qn in templateSpots:
-        #print('\n', qn['question'], '\n') 
-        for ans in qn['answers']:
-            #print(ans)
-
-
-            mask =np.zeros(alignedScanImgThresh.shape, dtype = "uint8")
-            cv2.circle(mask, tuple(ans['coord']), circleSize, 255, -1)        
-            maskPixels = cv2.countNonZero(mask)
-            mask = cv2.bitwise_and(alignedScanImgThresh, mask)
-            pctFilled = cv2.countNonZero(mask)/maskPixels
-
-            if pctFilled>=filledThreshold:
-                ans['filled']=True
-                cv2.circle(alignedScanImg, tuple(ans['coord']), circleSize, (0,0,255), 2)
-                #print(qn['question'], ans['value'], pctFilled)
-            else:
-                cv2.circle(alignedScanImg, tuple(ans['coord']), circleSize, (0,100,0), 2)
-            if pctFilled>0.1:
-                cv2.putText(alignedScanImg, str(round(pctFilled, 2)), tuple(ans['coord']),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
-    imomr.showImg(alignedScanImg)
-
-## return question:answer pairs
-
-    filledSpots ={'form':formPath.split("\\")[-1]}
-
-    for qn in templateSpots:    
-        filledSpots[str(qn['question'])] = []
-        for ans in qn['answers']:
-            if ans['filled']==True:
-                filledSpots[str(qn['question'])].append(ans['value'])
-
-    finalAnswers.append(filledSpots)
-
-## dict to dataframe (let the fun begin)
-
-omrOutput = pd.DataFrame(finalAnswers)
-
-
-def getDate(rw):
-     return "".join([str(i) for i in rw['day1']]) + "".join([str(i) for i in rw['day2']])  + '-' +"".join([str(i) for i in rw['month']])+ '-'+ "".join([str(i) for i in rw['year']])
-
-omrOutput['day']= omrOutput.apply(lambda x: getDate(x), axis=1)
-
-print(omrOutput)
-
-
-## or mimic csv output style from formscanner to allow reuse of analysis?
-
-## try align scanned image (fingers crossed!)
