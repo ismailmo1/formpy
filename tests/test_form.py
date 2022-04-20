81fe8e8c8320d761a4cac3e4be1700ab4e671c77
Ismail Mohammed <ismailmo4@gmail.com>
2022-04-20 18:17:03 +0100

    added form tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,27 +1,9 @@
-import cv2
+def test_answer_check_fill(form):
+    ans = form.questions[0].answers[20]
+    assert ans.is_filled(form.img) ==True
+    
+def test_answer_check_fill_perc(form):
+    ans = form.questions[1].answers[268]
+    fill_perc = ans.calc_filled_perc(form.img)
+    assert round(fill_perc,2) == 0.81
 
-
-def test_form_answer(form):
-    img = cv2.cvtColor(form.img, cv2.COLOR_GRAY2BGR)
-    answers = []
-    for question in form.questions:
-        for ans in question.answers:
-            if ans.is_filled(form.img):
-                answers.append(ans)
-                color = (0, 0, 255)
-                cv2.putText(
-                    img,
-                    str(question.question_id) + ans.value,
-                    (ans.x, ans.y),
-                    cv2.FONT_HERSHEY_COMPLEX,
-                    0.4,
-                    color,
-                )
-            else:
-                color = (0, 255, 0)
-
-            ans.mark_answer(img, color=color, circle_thickness=2)
-    cv2.imwrite("test.jpeg", img)
-
-# TODO generate new form template - simpler version of OEE? - run all tests on new template
-# TODO use gimp/googledocs? - future googledocs API integration to create new templates?
