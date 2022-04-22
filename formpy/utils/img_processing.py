53c6c1e4d033483c2481ce50c7dbb319bd3ce255
Ismail Mohammed <ismailmo4@gmail.com>
2022-04-22 18:52:48 +0100

    image processing docs

--- formpy/utils/img_processing.py
+++ formpy/utils/img_processing.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import cv2
 import numpy as np
 
@@ -6,12 +8,16 @@ class ImageAlignmentError(Exception):
     pass
 
 
-def show_img(img, time=0):
-    """
-    Img = single np.array or list of np.array imgs.
+def show_img(img: np.ndarray | list[np.ndarray], time: int = 0) -> None:
+    """Utility to display all images in a window with window name = window(i)
+    where i = index
 
-    displays all in window with window name = window(i) where i = index
+    Args:
+        img (np.ndarray | list[np.ndarray]): image to display as
+        array read into array e.g. via cv2.imread()
+        time (int, optional): Delay time in millisec to display window. Defaults to 0.
     """
+
     if type(img) != list:
         cv2.imshow("window", img)
     else:
@@ -22,9 +28,19 @@ def show_img(img, time=0):
         cv2.destroyAllWindows()
 
 
-def thresh_img(img, min_thresh=100, max_thresh=255):
-    """apply grayscale conversion and thresholding to image
-    returns image"""
+def thresh_img(
+    img: np.ndarray, min_thresh: int = 100, max_thresh: int = 255
+) -> np.ndarray:
+    """Convert image to binary black and white image using thresholds
+
+    Args:
+        img (np.ndarray): image to threshold read into array e.g. via cv2.imread()
+        min_thresh (int, optional): Pixels below this grayscale value will be converted to white pixels . Defaults to 100.
+        max_thresh (int, optional): Pixels above this grayscale value will be converted to black pixels. Defaults to 255.
+
+    Returns:
+        np.ndarray: thresholded image with only black or white pixels
+    """
     if len(img.shape) == 3 and img.shape[2] == 3:
         # three channels aka coloured img
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -38,10 +54,16 @@ def thresh_img(img, min_thresh=100, max_thresh=255):
     return img_thresh
 
 
-# img_processing.py
+def get_perspective_matrix(ordered_corner_pts: np.ndarray) -> np.ndarray:
+    """Calculates a matrix from the corners of the aligment feature
 
+    Args:
+        ordered_corner_pts (np.ndarray): Corner points of rectangle alignment feature,
+        ordered from top-left clockwise
 
-def get_perspective_matrix(ordered_corner_pts):
+    Returns:
+        np.ndarray: perspective matrix to use to align page using cv2.getPerspectiveTransform()
+    """
     # unpack ordered pts to find widths and heights
     (tl, tr, br, bl) = ordered_corner_pts
 
@@ -70,6 +92,18 @@ def get_perspective_matrix(ordered_corner_pts):
 
 
 def get_outer_box(img: np.ndarray) -> np.ndarray:
+    """Finds the rectangle alignment feature in the image
+
+    Args:
+        img (np.ndarray): image to detect the outer box from
+
+    Raises:
+        ImageAlignmentError: if outer box is not detected
+
+    Returns:
+        np.ndarray: returns the 4 coordinates of the corners of the rectangle alignment feature,
+        ordered from top-left clockwise
+    """
     # enhance image to improve contour detection
     if len(img.shape) == 3:
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -80,7 +114,9 @@ def get_outer_box(img: np.ndarray) -> np.ndarray:
 
     ##find outer rectangle
 
-    conts, _ = cv2.findContours(img_edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    conts, _ = cv2.findContours(
+        img_edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+    )
     # find top 10 biggest contours by area
     areas = []
     for c in conts:
@@ -103,23 +139,28 @@ def get_outer_box(img: np.ndarray) -> np.ndarray:
             # outer contour found with area method
             top_contours = area_sort[:5]
         else:
-            raise ImageAlignmentError("Image Alignment Failed: outer box not detected")
+            raise ImageAlignmentError(
+                "Image Alignment Failed: outer box not detected"
+            )
     elif area_cont_max_x < length_cont_max_x:
         if area_cont_max_y < length_cont_max_y:
             # outer contour found with length method
             top_contours = length_sort[:5]
         else:
-            raise ImageAlignmentError("Image Alignment Failed: outer box not detected")
+            raise ImageAlignmentError(
+                "Image Alignment Failed: outer box not detected"
+            )
 
     # find outer rectangle
     for c in top_contours:
-        # approximate the contour
         # approximate curve to check if its rectangularish
-        # check number of corners in apprximated polygon
         approx = cv2.approxPolyDP(c[0], 0.0015 * c[2], True)
+        # check number of corners in apprximated polygon
         if len(approx) >= 4:
             pts = np.zeros((4, 2), dtype="float32")
+            # top left
             pts[0] = approx[np.argmin(approx.sum(axis=2))]
+            # top right
             pts[2] = approx[np.argmax(approx.sum(axis=2))]
 
             # smallest difference x-y = top right
@@ -128,20 +169,23 @@ def get_outer_box(img: np.ndarray) -> np.ndarray:
             pts[3] = approx[np.argmax(np.diff(approx, axis=2))]
 
             break
-    ## debug snippet to show pts on img
-    # color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    # color_img = img
-    # for p in pts:
-    #     cv2.circle(color_img, (int(p[0]), int(p[1])), 10, (0, 0, 255), -1)
-    # show_img(color_img)
-    # color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
     return pts
 
 
-def align_page(img: np.ndarray, corner_pts=None):
-    """apply perspective transformation to align form with template
-    img = input form as np.array (use cv2.imread)
-    width = use template imagmage, will override width and height with euclidean distances of outer border"""
+def align_page(
+    img: np.ndarray, corner_pts: np.ndarray | None = None
+) -> np.ndarray:
+    """Applys perspective transform to align the image using a rectangle alignment feature on the image
+
+    Args:
+        img (np.ndarray): image of form or template read into array e.g. via cv2.imread()
+        corner_pts (np.ndarray | None, optional): 4 coordinates of the corners of the rectangle alignment feature,
+        ordered from top-left clockwise. Defaults to None, and the rectangle feature will be detected automatically.
+
+    Returns:
+        np.ndarray: aligned image
+    """
 
     if corner_pts is not None:
         ordered_pts = corner_pts
@@ -159,7 +203,8 @@ def align_page(img: np.ndarray, corner_pts=None):
     # transform image and resize to original size (map spots to correct locations)
     img_warp = cv2.warpPerspective(img, matrix, (width, height))
 
-    # TODO add rotation feature (separate func probably) - add detection mechanism for choosing rotation e.g. top left spot or something
+    # TODO add rotation feature (separate func probably) -
+    # TODO add detection mechanism for choosing rotation e.g. top left spot or something
 
     # if img_warp.shape[0] > img_warp.shape[1]:
     #     img_warp = cv2.rotate(img_warp, cv2.ROTATE_90_CLOCKWISE)
@@ -167,7 +212,16 @@ def align_page(img: np.ndarray, corner_pts=None):
     return img_warp
 
 
-def process_img(img: np.ndarray):
+def process_img(img: np.ndarray) -> np.ndarray:
+    """Converts image to binary black & white and aligns the page using the rectangle alignment feature
+
+    Args:
+        img (np.ndarray): image read into array e.g. via cv2.imread()
+
+    Returns:
+        np.ndarray: binary black and white, aligned image
+    """
+
     img_thresh = thresh_img(img)
     img_aligned = align_page(img_thresh)
     return img_aligned
