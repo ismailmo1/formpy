44a555ab0334a30015ab9805e2a536c5525a6b28
Ismail Mohammed <ismailmo4@gmail.com>
2022-04-22 17:38:33 +0100

    form init cleanup

--- formpy/form.py
+++ formpy/form.py
@@ -11,45 +11,63 @@ from .template import Template
 
 
 class Form:
-    """Class to represent a single paged form containing questions."""
-
     def __init__(self, img: np.ndarray, template: Template) -> Form:
-        """Form Class with associated template.
+        """Initialise form with an associated template that it was built from
 
         Args:
-            width (int): width of image, i.e. np.ndarray.size[0]
-            height (int): height of image, i.e. np.ndarray.size[1]
-            questions (list): list of Question instances
+            img (np.ndarray): a form image read into array e.g. through cv2.imread()
+            template (Template): template that the form was built from
+
+        Returns:
+            Form
         """
         self.template = template
-        # resize processed image to same as template
-        self.img = cv2.resize(
-            ip.process_img(img),
-            (template.img.shape[1], template.img.shape[0]),
-            interpolation=cv2.INTER_LINEAR,
-        )
+        self.img = self.__resize_img(img)
         self.questions = template.questions
 
     def __repr__(self) -> str:
         return f"Form with {len(self.questions)} questions and {sum([len(i.answers) for i in self.questions])}"
 
-    def mark_all_answers(
-        self, found_answers: list, color: Tuple[int] = (0, 0, 255)
-    ) -> np.ndarray:
-        color_img = cv2.cvtColor(cv2.bitwise_not(self.img), cv2.COLOR_GRAY2BGR)
-        for qn in found_answers:
-            answers = found_answers[qn]
+    def __resize_img(self, img: np.ndarray) -> np.ndarray:
+        """resize image to be of same size as template
+
+        Args:
+            img (np.ndarray): form image read into array e.g. through cv2.imread()
+        """
+        processed_img = ip.process_img(img)
+        resized_img = cv2.resize(
+            processed_img,
+            (self.template.img.shape[1], self.template.img.shape[0]),
+            interpolation=cv2.INTER_LINEAR,
+        )
+        return resized_img
+
+    def mark_all_answers(self, colour: Tuple[int] = (0, 0, 255)) -> np.ndarray:
+        """mark all answers on the form image with the question id and answer value
+
+        Args:
+            colour (Tuple[int], optional): color of text and circles to mark answers in BGR. Defaults to (0, 0, 255).
+
+        Returns:
+            np.ndarray: form image with marked answers, answer values and question ids
+        """
+        colour_img = cv2.cvtColor(
+            cv2.bitwise_not(self.img), cv2.COLOR_GRAY2BGR
+        )
+
+        for qn in self.template.questions:
+            answers = qn.answers
             for ans in answers:
 
                 cv2.putText(
-                    color_img,
+                    colour_img,
                     str(qn.question_id) + ans.value,
                     (ans.x, ans.y),
                     cv2.FONT_HERSHEY_COMPLEX,
                     0.4,
-                    color,
+                    colour,
                 )
 
-                ans.mark_answer(color_img, color=color, circle_thickness=-1)
+                ans.mark_answer(colour_img, colour=colour, circle_thickness=-1)
 
-        return color_img
+        return colour_img
